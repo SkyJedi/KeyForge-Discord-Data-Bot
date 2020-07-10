48731299a5fc771b5eab697cbc633ae79363ed23
skyjedi@gmail.com <4tX7wP7kYh*8>
2020-07-10 08:47:42 -0500

    # 1.6.2     better card frames

--- functions/timingChart.js
+++ functions/timingChart.js
@@ -15,15 +15,17 @@ const timingChart = async (msg, params) => {
             .setDescription(format(data.steps));
     } else {
         embed.setTitle('TIMING CHART')
-            .addField('1) FORGE A KEY', '*» START OF TURN EFFECTS TRIGGER.*\n1. Check to see if you are able to forge a key.\n2. If able, spend Æmber equal to the current forging cost and forge a key. If you forge your 3rd key, you immediately win the game.\n*» AFTER A KEY IS FORGED EFFECTS TRIGGER*')
+            .addField('Start of Turn', '*» START OF TURN EFFECTS TRIGGER.*')
+            .addField('1) FORGE A KEY', '1. Check to see if you are able to forge a key.\n2. If able, spend Æmber equal to the current forging cost and forge a key. If you forge your 3rd key, you immediately win the game.\n*» AFTER A KEY IS FORGED EFFECTS TRIGGER*')
             .addField('2) CHOOSE A HOUSE', '1. Choose which house will be the active house for this turn.\n*» AFTER YOU CHOOSE A HOUSE EFFECTS TRIGGER.*\n2. You may take all the cards from your archives and put them in your hand.')
             .addField('3) PLAY, DISCARD, OR USE CARDS', 'You may perform these actions in any order and repeat them any number of times.')
-            .addField('PLAY A CARD.', '1. If your card is a creature, artifact or upgrade, put it into play. If your card is an action, reveal it (after you play it, discard it).\n2. Resolve the Æmber bonus on your card.\n*» PLAY: EFFECTS AND AFTER PLAY/ENTERS PLAY EFFECTS TRIGGER.*')
+            .addField('PLAY A CARD.', '1. If your card is a creature, artifact or upgrade, put it into play. If your card is an action, reveal it (after you play it, discard it).\n2. Resolve each Bonus Icon on your card, one at a time.\n*» PLAY: EFFECTS AND AFTER PLAY/ENTERS PLAY EFFECTS TRIGGER.*')
             .addField('USE A CREATURE TO FIGHT.', '1. Exhaust the attacking creature and choose the creature they are fighting.\n*» BEFORE FIGHT, HAZARDOUS X, AND ASSAULT X TRIGGER.*\n2. Creatures deal their damage to each other simultaneously.\n*» FIGHT: EFFECTS, AFTER A CREATURE FIGHTS EFFECTS, AND AFTER A CREATURE IS USED EFFECTS TRIGGER.*')
             .addField('USE A CREATURE TO REAP.', '1. Exhaust the reaping creature.\n2. Gain 1 Æmber.\n*» REAP: EFFECTS, AFTER A CREATURE REAPS EFFECTS, AND *AFTER A CREATURE IS USED& EFFECTS TRIGGER.*')
             .addField('USE AN ACTION OR OMNI ABILITY.', '1. Exhaust your creature or artifact with the Action: or Omni: ability.\n2. Resolve the effects of that ability.\n*» AFTER A CREATURE IS USED EFFECTS TRIGGER.*')
             .addField('4) READY CARDS', '1. Ready each of your exhausted cards')
-            .addField('5) DRAW CARDS', '1. Draw cards until you have six or more in your hand (adjusting for chains or card effects).\n*» END OF TURN EFFECTS TRIGGER.*');
+            .addField('5) DRAW CARDS', '1. Draw cards until you have six or more in your hand (adjusting for chains or card effects).')
+            .addField('End of Turn', '*» “END OF TURN” EFFECTS TRIGGER.*');
     }
     main.sendMessage(msg, { embed });
 };
